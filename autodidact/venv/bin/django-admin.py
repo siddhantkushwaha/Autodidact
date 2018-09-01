adc14c283e33e9f0fbc3211b5af3c48ad6a6f08b
udayrajsawhney <udayraj.s16@iiits.in>
2018-09-01 10:38:22 +0530

    server-digitalocean

---
+++ autodidact/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/udaysawhney/Desktop/project/autodidact_forum/autodidact/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
