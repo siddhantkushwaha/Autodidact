83931830685081c88a292dae062d2f30107f1006
Siddhant Kushwaha <siddhant.k16@iiits.in>
2018-09-02 23:55:03 +0530

    models created

--- autodidact/app/admin.py
+++ autodidact/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Tag, Thread, Answer, Comment
 
-# Register your models here.
+admin.site.register(Tag)
+admin.site.register(Thread)
+admin.site.register(Answer)
+admin.site.register(Comment)
+
--- autodidact/app/models.py
+++ autodidact/app/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    use_count = models.IntegerField(default=0)
+    creation_time = models.TimeField()
+
+    def __str__(self):
+        return str(self.name) + ' ;' + str(self.use_count) + ' ;' + str(self.creation_time)
+
+
+class Thread(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100000)
+    view_count = models.IntegerField(default=0)
+    upvote_count = models.IntegerField(default=0)
+    downvote_count = models.IntegerField(default=0)
+    creation_time = models.TimeField()
+
+    def __str__(self):
+        return str(self.title) + ' ;' + str(self.creation_time)
+
+
+class Answer(models.Model):
+    description = models.CharField(max_length=100000)
+    upvote_count = models.IntegerField(default=0)
+    downvote_count = models.IntegerField(default=0)
+    creation_time = models.TimeField()
+
+    def __str__(self):
+        return str(self.description)[:20] + ' ;' + str(self.creation_time)
+
+
+class Comment(models.Model):
+    description = models.CharField(max_length=100)
+    creation_time = models.TimeField()
+
+    def __str__(self):
+        return str(self.description)[:20] + ' ;' + str(self.creation_time)
