32657be46ad1b2aa893d61903be3db03447f3c36
Siddhant Kushwaha <siddhant.k16@iiits.in>
2018-09-03 00:36:32 +0530

    basic

--- autodidact/app/models.py
+++ autodidact/app/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='UserProfile')
+
+    def __str__(self):
+        return self.user.username
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=20)
     use_count = models.IntegerField(default=0)
-    creation_time = models.TimeField()
+    creation_time = models.TimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.name) + ' ;' + str(self.use_count) + ' ;' + str(self.creation_time)
@@ -16,7 +24,7 @@ class Thread(models.Model):
     view_count = models.IntegerField(default=0)
     upvote_count = models.IntegerField(default=0)
     downvote_count = models.IntegerField(default=0)
-    creation_time = models.TimeField()
+    creation_time = models.TimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.title) + ' ;' + str(self.creation_time)
@@ -26,7 +34,7 @@ class Answer(models.Model):
     description = models.CharField(max_length=100000)
     upvote_count = models.IntegerField(default=0)
     downvote_count = models.IntegerField(default=0)
-    creation_time = models.TimeField()
+    creation_time = models.TimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.description)[:20] + ' ;' + str(self.creation_time)
@@ -34,7 +42,7 @@ class Answer(models.Model):
 
 class Comment(models.Model):
     description = models.CharField(max_length=100)
-    creation_time = models.TimeField()
+    creation_time = models.TimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.description)[:20] + ' ;' + str(self.creation_time)
