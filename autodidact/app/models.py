dbc53ce6bb2929d68603e740914ab50baf333e31
udayrajsawhney <udayraj.s16@iiits.in>
2018-10-21 19:33:20 +0530

    oct 21

--- autodidact/app/models.py
+++ autodidact/app/models.py
@@ -19,11 +19,12 @@ class Tag(models.Model):
 
 class Thread(models.Model):
     title = models.CharField(max_length=100)
-    description = models.IntegerField()
+    description = models.CharField(max_length=500, null=True)
     view_count = models.IntegerField(default=0)
     upvote_count = models.IntegerField(default=0)
     downvote_count = models.IntegerField(default=0)
     creation_time = models.TimeField(auto_now_add=True)
+    created_by = models.ForeignKey(ForumUser, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return str(self.title) + ' ;' + str(self.creation_time)
@@ -34,6 +35,7 @@ class Answer(models.Model):
     upvote_count = models.IntegerField(default=0)
     downvote_count = models.IntegerField(default=0)
     creation_time = models.TimeField(auto_now_add=True)
+    created_by = models.ForeignKey(ForumUser, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return str(self.description)[:20] + ' ;' + str(self.creation_time)
@@ -42,6 +44,7 @@ class Answer(models.Model):
 class Comment(models.Model):
     description = models.CharField(max_length=100)
     creation_time = models.TimeField(auto_now_add=True)
+    created_by = models.ForeignKey(ForumUser, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return str(self.description)[:20] + ' ;' + str(self.creation_time)
