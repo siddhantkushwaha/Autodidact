e40c5a9ab232643c6b22bb57fa3f3a583c518101
Siddhant Kushwaha <>
2018-10-26 11:41:17 +0530

    new branch

--- autodidact/app/requests.py
+++ autodidact/app/requests.py
@@ -9,5 +9,5 @@ def auth_api(token):
         'secret': '40e05b687fe391deef99f10aced9ebc9096cd0410190ae5ef4fc797c02df9ce0cf7455b3be59baa0810e855dce66f80f4cb81d0a2d84dfa95877d46c3c15c861'
     })
 
-    print(res)
+    print(res, 'here')
     return 'siddhant.k16@iiits.in', 'siddhant@1234'
