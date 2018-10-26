37c2f4c0ec2aed32bbe8a689341610e34659886c
Siddhant Kushwaha <>
2018-10-26 09:01:11 +0530

    fixing the models

--- autodidact/app/models.py
+++ autodidact/app/models.py
@@ -14,7 +14,7 @@ class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
     use_count = models.IntegerField(default=0)
     creation_time = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(ForumUser, on_delete=models.SET_NULL, null=True)
+    created_by = models.ForeignKey(ForumUser)
 
     def __str__(self):
         return str(self.name) + ' ;' + str(self.created_by)
@@ -22,13 +22,13 @@ class Tag(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=1000, null=True)
+    description = models.CharField(max_length=1000)
     tags = models.ManyToManyField(Tag)
     view_count = models.IntegerField(default=0)
     upvote_count = models.IntegerField(default=0)
     downvote_count = models.IntegerField(default=0)
     creation_time = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(ForumUser, on_delete=models.SET_NULL, null=True)
+    created_by = models.ForeignKey(ForumUser)
 
     def __str__(self):
         return str(self.title) + ' ;' + str(self.creation_time)
@@ -40,7 +40,7 @@ class Answer(models.Model):
     upvote_count = models.IntegerField(default=0)
     downvote_count = models.IntegerField(default=0)
     creation_time = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(ForumUser, on_delete=models.SET_NULL, null=True)
+    created_by = models.ForeignKey(ForumUser)
 
     def __str__(self):
         return str(self.description)[:20] + ' ;' + str(self.creation_time)
@@ -51,7 +51,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, null=True)
     answer = models.ForeignKey(Answer, null=True)
     creation_time = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(ForumUser, on_delete=models.SET_NULL, null=True)
+    created_by = models.ForeignKey(ForumUser)
 
     def __str__(self):
         return str(self.description)[:20] + ' ;' + str(self.creation_time)
