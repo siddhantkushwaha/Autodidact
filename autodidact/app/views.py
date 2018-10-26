7e75cab2d401a0650d892db51962b88f5dcd5e32
Siddhant Kushwaha <>
2018-10-26 12:38:19 +0530

    fix 4

--- autodidact/app/views.py
+++ autodidact/app/views.py
@@ -47,18 +47,20 @@ def login_user(request, token):
 
 
 def auth_api(token):
-    res = requests.post(url=' https://serene-wildwood-35121.herokuapp.com/oauth/getDetails', data={
-        'token': token,
-        'secret': '40e05b687fe391deef99f10aced9ebc9096cd0410190ae5ef4fc797c02df9ce0cf7455b3be59baa0810e855dce66f80f4cb81d0a2d84dfa95877d46c3c15c861'
-    })
+    try:
+        res = requests.post(url=' https://serene-wildwood-35121.herokuapp.com/oauth/getDetails', data={
+            'token': token,
+            'secret': '40e05b687fe391deef99f10aced9ebc9096cd0410190ae5ef4fc797c02df9ce0cf7455b3be59baa0810e855dce66f80f4cb81d0a2d84dfa95877d46c3c15c861'
+        })
+        res = json.loads(res.content)
+        email = res['student'][0]['Student_Email']
+        password = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
 
-    res = json.loads(res.content)
-    email = res['student'][0]['Student_Email']
-    password = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+        print(email, password)
+        return email, password
 
-    print(email, password)
-
-    return email, password
+    except Exception as e:
+        print(e)
 
 
 def get_forum_user(email, password):
