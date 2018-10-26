797458a074a52760804540c96653bbe02ceba909
Siddhant Kushwaha <>
2018-10-26 12:25:15 +0530

    url fix

--- autodidact/app/urls.py
+++ autodidact/app/urls.py
@@ -7,7 +7,7 @@ app_name = 'app'
 
 urlpatterns = [
     url(r'^$', views.main, name='main'),
-    url(r'^(?P<token>[\w, .]+)$', views.login_user, name='login'),
+    url(r'^(?P<token>.+)$', views.login_user, name='login'),
     url(r'^profile$', views.user_profile, name='userProfile'),
 
     url(r'^posts$', views.get_posts, name='posts'),
