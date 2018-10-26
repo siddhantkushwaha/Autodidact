8561bb20c98004b5da64796b7ede741daeb72c0e
Siddhant Kushwaha <>
2018-10-26 11:51:45 +0530

    fix 2

--- autodidact/app/requests.py
+++ autodidact/app/requests.py
@@ -4,10 +4,10 @@ import requests
 def auth_api(token):
     print('done')
 
-    res = requests.post(url='http://127.0.0.1:8000/add/tag', data={
-        'token': token,
-        'secret': '40e05b687fe391deef99f10aced9ebc9096cd0410190ae5ef4fc797c02df9ce0cf7455b3be59baa0810e855dce66f80f4cb81d0a2d84dfa95877d46c3c15c861'
-    })
+    # res = requests.post(url='http://127.0.0.1:8000/add/tag', data={
+    #     'token': token,
+    #     'secret': '40e05b687fe391deef99f10aced9ebc9096cd0410190ae5ef4fc797c02df9ce0cf7455b3be59baa0810e855dce66f80f4cb81d0a2d84dfa95877d46c3c15c861'
+    # })
 
-    print(res, 'here')
+    # print(res, 'here')
     return 'siddhant.k16@iiits.in', 'siddhant@1234'
