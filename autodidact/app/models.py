506e3b927d2df0f1c893749caff9737bad5eae9d
Siddhant Kushwaha <k16.siddhant@gmail.com>
2018-11-10 23:21:47 +0530

    model changed

--- autodidact/app/models.py
+++ autodidact/app/models.py
@@ -21,7 +21,7 @@ class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
     use_count = models.IntegerField(default=0)
     creation_time = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(ForumUser)
+    created_by = models.ForeignKey('ForumUser')
 
     def __str__(self):
         return str(self.name) + ' ;' + str(self.created_by)
@@ -35,12 +35,13 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
     tags = models.ManyToManyField(Tag)
-    up_voters = models.ManyToManyField(ForumUser, related_name='up_voted_post')
-    down_voters = models.ManyToManyField(ForumUser, related_name='down_voted_post')
-    viewers = models.ManyToManyField(ForumUser, related_name='viewed_post')
+    accepted_answer = models.OneToOneField('Answer', null=True, related_name='accepted_answer')
+    up_voters = models.ManyToManyField('ForumUser', related_name='up_voted_post')
+    down_voters = models.ManyToManyField('ForumUser', related_name='down_voted_post')
+    viewers = models.ManyToManyField('ForumUser', related_name='viewed_post')
     closed = models.BooleanField(default=False)
     creation_time = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(ForumUser, related_name='created_post')
+    created_by = models.ForeignKey('ForumUser', related_name='created_post')
 
     def __str__(self):
         return str(self.title) + ' ;' + str(self.creation_time)
@@ -52,12 +53,11 @@ particular post, it is dependant on it hence a foreign key of the Post Class.'''
 
 class Answer(models.Model):
     description = models.CharField(max_length=1000)
-    post = models.ForeignKey(Post)
-    up_voters = models.ManyToManyField(ForumUser, related_name='up_voted_answer')
-    down_voters = models.ManyToManyField(ForumUser, related_name='down_voted_answer')
-    accepted = models.BooleanField(default=False)
+    post = models.ForeignKey('Post')
+    up_voters = models.ManyToManyField('ForumUser', related_name='up_voted_answer')
+    down_voters = models.ManyToManyField('ForumUser', related_name='down_voted_answer')
     creation_time = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(ForumUser, related_name='created_answer')
+    created_by = models.ForeignKey('ForumUser', related_name='created_answer')
 
     def __str__(self):
         return str(self.description)[:20] + ' ;' + str(self.creation_time)
@@ -69,10 +69,10 @@ for a particular post or answer, it is dependant on it hence a foreign key exist
 
 class Comment(models.Model):
     description = models.CharField(max_length=100)
-    post = models.ForeignKey(Post, null=True)
-    answer = models.ForeignKey(Answer, null=True)
+    post = models.ForeignKey('Post', null=True)
+    answer = models.ForeignKey('Answer', null=True)
     creation_time = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(ForumUser, related_name='created_comment')
+    created_by = models.ForeignKey('ForumUser', related_name='created_comment')
 
     def __str__(self):
         return str(self.description)[:20] + ' ;' + str(self.creation_time)
